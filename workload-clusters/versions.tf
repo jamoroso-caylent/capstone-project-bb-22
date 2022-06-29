67db40cf34a96309c29a7f460590f34a96ca89ed
jamoroso-caylent <jose.amoroso@caylent.com>
2022-06-29 15:18:56 -0500

    Modified version to add constraints

--- workload-clusters/versions.tf
+++ workload-clusters/versions.tf
@@ -7,15 +7,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.72"
+      version = ">= 3.72, <= 4.18.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.10"
+      version = ">= 2.10, < 2.12"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.4.1"
+      version = ">= 2.4.1, <= 2.5.1"
     }
   }
 
