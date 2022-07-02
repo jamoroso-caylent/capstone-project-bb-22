5959209af8f1199da336e86bdeb6699291ce3991
jamoroso-caylent <jose.amoroso@caylent.com>
2022-07-01 22:44:54 -0500

    Added frontend pipeline

--- workload-clusters/codepipeline.tf
+++ workload-clusters/codepipeline.tf
@@ -8,6 +8,16 @@ module "app1-backend-cicd" {
   tags                    = local.tags
 }
 
+module "app1-frontend-cicd" {
+  source                  = "./modules/codepipeline"
+  name_prefix             = "simple-crud-frontend-app"
+  repository_id           = "jamoroso-caylent/spring-boot-angular-14-mysql-example"
+  repository_branch       = "master"
+  buildspec_path          = "angular-14-client/buildspec.yml"
+  codestar_connection_arn = aws_codestarconnections_connection.codestar_connection.arn
+  tags                    = local.tags
+}
+
 resource "aws_codestarconnections_connection" "codestar_connection" {
   name          = "bb-2022-connection"
   provider_type = "GitHub"
